6c0d1b36ee52b826764842fda0a09a633880a86d
Ivan <ivan.grubisic.mail@gmail.com>
2018-03-15 19:51:32 +0100

    Removed binary files

---
+++ Deno/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Deno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bottomBar = new System.Windows.Forms.StatusStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runOnStartupCB = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new DenoTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.panel1.SuspendLayout();
+            this.bottomBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.bottomBar);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 2, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(192, 137);
+            this.panel1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.bottomBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.bottomBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.bottomBar.Location = new System.Drawing.Point(0, 115);
+            this.bottomBar.Name = "statusStrip1";
+            this.bottomBar.Size = new System.Drawing.Size(192, 22);
+            this.bottomBar.SizingGrip = false;
+            this.bottomBar.TabIndex = 1;
+            this.bottomBar.Text = "statusStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem,
+            this.runOnStartupCB});
+            this.toolStripDropDownButton1.ForeColor = System.Drawing.Color.LightGray;
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.ShowDropDownArrow = false;
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(42, 20);
+            this.toolStripDropDownButton1.Text = "Menu";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // runOnStartupCB
+            // 
+            this.runOnStartupCB.CheckOnClick = true;
+            this.runOnStartupCB.Name = "runOnStartupCB";
+            this.runOnStartupCB.Size = new System.Drawing.Size(152, 22);
+            this.runOnStartupCB.Text = "Run on startup";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox1.Location = new System.Drawing.Point(0, 2);
+            this.textBox1.MaxLength = 2047;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 135);
+            this.textBox1.TabIndex = 1;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Deno";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(192, 137);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.bottomBar.ResumeLayout(false);
+            this.bottomBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private DenoTextBox textBox1;
+        private System.Windows.Forms.StatusStrip bottomBar;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runOnStartupCB;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Deno/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Deno
+{
+    public partial class Form1 : Form
+    {
+        class RegistryProperties
+        {
+            private static readonly RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            private static readonly string appName = Application.ProductName.ToString();
+
+            private bool _runOnStartup = !string.IsNullOrEmpty((string)rk.GetValue(appName));
+
+            public Boolean RunOnStartup
+            {
+                get => _runOnStartup;
+                set
+                {
+                    _runOnStartup = value;
+                    if (value) rk.SetValue(appName, Application.ExecutablePath.ToString());
+                    else rk.DeleteValue(appName, false);
+                }
+            }
+        }
+
+        private RegistryProperties registryProperties = new RegistryProperties();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // window dragging
+            this.bottomBar.MouseDown += (object s, MouseEventArgs e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                    GuiHelper.StartDragging(this);
+            };
+
+            // bottom bar visibility
+            this.textBox1.KeyUp += (s, e) => bottomBar.Visible = false;
+            this.Activated += (s, e) => bottomBar.Visible = true;
+            //this.textBox1.Click += (s, e) => bottomBar.Visible = true;
+            this.Deactivate += (s, e) => bottomBar.Visible = false;
+
+            // text, position and window size saving
+            this.Deactivate += (s, e) => SaveProperties();
+            this.FormClosing += (s, e) => SaveProperties();
+
+            // notification area icon actions
+            this.notifyIcon1.Click += (s, e) => this.Activate();
+
+            // menu actions
+            this.closeToolStripMenuItem.Click += (s, e) => this.Close();
+            
+            // menu->run-on-startup checkbox 
+            runOnStartupCB.Checked = registryProperties.RunOnStartup;
+            this.runOnStartupCB.CheckedChanged += (s, e) => registryProperties.RunOnStartup = runOnStartupCB.Checked;
+
+            // state loading
+            RestoreProperties();
+            this.textBox1.Select(int.MaxValue, 0);  // unselect text and move cursor to the end
+        }
+
+        private void RestoreProperties()
+        {
+            var a = new Properties.Settings();
+            this.Size = a.WindowSize;
+            this.SetDesktopLocation(a.WindowLocation.X, a.WindowLocation.Y);
+            textBox1.Text = a.Text;
+        }
+
+        private void SaveProperties() =>
+            new Properties.Settings
+            {
+                WindowSize = this.Size,
+                WindowLocation = this.PointToScreen(new System.Drawing.Point(0, 0)),
+                Text = textBox1.Text
+            }.Save();
+    }
+}
